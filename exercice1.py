d25627dfdd8d1f2af7acaf52fa48198a30aa583b
Maxime Imbeau <maxime.imbeau.1@ulaval.ca>
2020-11-11 14:26:40 -0500

    exercice1 completed

--- exercice1.py
+++ exercice1.py
@@ -1,43 +1,53 @@
 import re
-var = input (" Donnez une valeur a var : ")
-if re . search (r" World ", var ):
-print (" var contient le mot World ")
+
+
+var = input("Donnez une valeur a var : ")
+
+if re.search(r"World", var):
+    print("var contient le mot World")
+else :  
+    print("var ne contient pas le mot World")
+if re.search (r".", var):
+    print ("var contient au moins un caractere")
 else :
-print (" var ne contient pas le mot World ")
-if re . search (r".", var ):
-print (" var contient au moins un caractere ")
+    print ("var ne contient pas de caracteres")
+if re.search (r"^.?$", var):
+    print (" var contient un ou zero caractere ")
 else :
-print (" var ne contient pas de caracteres ")
-if re . search (r" ^.? $", var ):
-print (" var contient un ou zero caractere ")
-else :
-print (" var contient plus dun caractere ")
+    print (" var contient plus dun caractere ")
 if re . search (r"\d {3}", var ):
-print (" var contient trois chiffres de suite ")
+    print (" var contient trois chiffres de suite ")
 else :
-print (" var ne contient pas trois chiffres de suite ")
-1
-if re . search (r"[a-zA -Z0 -9]{3 ,12} ", var ):
-print (" var contient entre 3 et 12 caracteres alphanumeriques de suite ")
+    print (" var ne contient pas trois chiffres de suite ")
+if re.search(r"[a-zA-Z0-9]{3 ,12}", var):
+    print (" var contient entre 3 et 12 caracteres alphanumeriques de suite ")
 else :
-print (" var ne contient pas entre 3 et 12 caracteres alphanumeriques de suite ")
-if re . search (r"[ TWHQ ] ello ", var ):
-print (" var contient un T, un W, un H ou un Q suivi de ello ")
+    print (" var ne contient pas entre 3 et 12 caracteres alphanumeriques de suite ")
+if re . search (r"[TWHQ]ello", var ):
+    print (" var contient un T, un W, un H ou un Q suivi de ello ")
 else :
-print (" var ne contient pas un T, un W, un H ou un Q suivi de ello ")
-if re . search (r" foo | bar | ello ", var ):
-print (" var contient foo , bar ou ello ")
+    print (" var ne contient pas un T, un W, un H ou un Q suivi de ello ")
+if re . search (r"foo|bar|ello", var):
+    print (" var contient foo , bar ou ello ")
 else :
-print (" var ne contient pas foo , bar ou ello ")
-if re . search (r"^( Hello )", var ):
-print (" var commence par Hello ")
+    print (" var ne contient pas foo , bar ou ello ")
+if re . search (r"^(Hello)", var ):
+    print (" var commence par Hello ")
 else :
-print (" var ne commence pas par Hello ")
-if re . search (r" !!![0 -9]{3} $", var ):
-print (" var se termine par !!! suivi de 3 chiffres ")
+    print (" var ne commence pas par Hello ")
+if re . search (r"!!![0-9]{3}$", var ):
+    print (" var se termine par !!! suivi de 3 chiffres ")
 else :
-print (" var ne se termine pas par !!! suivi de 3 chiffres ")
-if re . search (r"^H .*[0 -9]{3} $", var ):
-print (" var commence par un H et se termine par 3 chiffres ")
+    print (" var ne se termine pas par !!! suivi de 3 chiffres ")
+if re . search (r"^H.*[0-9]{3}$", var ):
+    print (" var commence par un H et se termine par 3 chiffres ")
 else :
-print (" var ne commence pas par un H ou ne se termine pas par 3 chiffres ")
+    print("var ne commence pas par un H ou ne se termine pas par 3 chiffres")
+if re.search(r"^[^@]+@[^@]+\.[^@]+$", var):
+    print ("var est un courriel valide")
+else :
+    print ("var nest pas un courriel valide")
+if re.search (r"(?=[^a-z]*[a-z])(?=[^\d]*\d{3})", var):
+    print (" var contient une lettre et trois chiffres de suite ( peu importe l’ordre )")
+else :
+    print (" var ne contient pas de lettre ou trois chiffres de suite ")
